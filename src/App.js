79db543c0ea5fcfc061ba3702f8ce21cd7441b8c
gauravprakash <ergauravprakash@gmail.com>
2022-06-03 12:15:13 +0530

    initial routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.scss';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/movie/:imdbID' element={<MovieDetail />}/>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
