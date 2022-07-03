d5eb9257e813b763a2bd87a755df19e2a66afb2b
gauravprakash <ergauravprakash@gmail.com>
2022-07-03 22:30:44 +0530

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='footer'>
       <div>Movie Hub</div>
       <div> 
-        No &#169;, API used: <a href='http://www.omdbapi.com' target="_blank">http://www.omdbapi.com</a> by ~Gaurav Prakash <i className="fa fa-envelope" aria-hidden="true"></i> : <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=ergauravprakash@gmail.com" target="_blank"><i>ergauravprakash@gmail.com</i></a>
+        No &#169;, API used: <a href='http://www.omdbapi.com' target="_blank" rel="noreferrer">http://www.omdbapi.com</a> by ~Gaurav Prakash <i className="fa fa-envelope" aria-hidden="true"></i> : <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=ergauravprakash@gmail.com" target="_blank" rel="noreferrer"><i>ergauravprakash@gmail.com</i></a>
       </div>
     </div>
   )
